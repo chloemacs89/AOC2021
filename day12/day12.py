5a2b59c3e6323d0ff286ac178477d830ade1589d
Chloë VILLEY <chloe.villey89@gmail.com>
2021-12-13 00:42:10 +0100

    2nd star... might need some eyebleach after looking at this code.

--- day12/day12.py
+++ day12/day12.py
@@ -10,7 +10,6 @@ with open("/home/roflzyo/aoc/2021/day12/input.txt") as f:
             graph[w] += [v]
         else:
             graph[w] = [v]
-    
 
 mini_data = {
     "start": ["A", "b"],
@@ -21,20 +20,36 @@ mini_data = {
 }
 
 
-def make_paths(graph, start, end, path, paths):
+def make_paths(graph, start, end, path, paths, twice=False):
+    if start.islower() and start in path:
+        path.append(1)
     if start == end:
         return path + [start]
     for node in graph[start]:
-        if node in path and node.islower():
+        if twice and node in path and node.islower() and 1 in path:
+            pass
+        elif twice and node in path and node.islower() and path.count(node) > 1:
+            path.append(1)
+            pass
+        elif node in path and node.islower() and not twice:
             pass
         elif node == "start":
             pass
         else:
-            newpath = make_paths(graph, node, end, path+[start], paths)
+            newpath = make_paths(graph, node, end, path + [start], paths,
+                                 twice)
             if newpath:
                 paths.append(newpath)
 
 
-paths = []            
+paths = []
+# make_paths(mini_data, "start", "end", [], paths)
 make_paths(graph, "start", "end", [], paths)
 print(len(paths))
+
+paths = []
+# make_paths(mini_data, "start", "end", [], paths, twice=True)
+make_paths(graph, "start", "end", [], paths, twice=True)
+print(len(paths))
+
+
